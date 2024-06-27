66d21cdb1eeba4d33b6cef39c178026b1015598c
PNgoDuongthe <130558451+PNgoDuongthe@users.noreply.github.com>
2024-06-27 13:36:57 +0700

    Arrays

---
+++ Arrays/Array_List.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.ArrayList;
+
+public class Array_List {
+    public static void main(String[] args) {
+        ArrayList<String> VietnameseFood = new ArrayList<String>();
+        VietnameseFood.add("Banh mi");
+        VietnameseFood.add("Pho");
+        VietnameseFood.add("Bun cha");
+        VietnameseFood.add("Cha ca");
+//        VietnameseFood.set(0,"Com");
+//        VietnameseFood.remove("Cha ca");
+//        VietnameseFood.clear();
+        for(int i=0;i<VietnameseFood.size();i++)
+        {
+            System.out.println(VietnameseFood.get(i));
+        }
+    }
+}
